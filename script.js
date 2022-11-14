98cd7648abcf67769208f6ae8f007ba9df944889
Eros Ravier S. Aragão <deverosravier.15@gmail.com>
2022-11-14 06:08:38 -0300

    atualizar com scroll
    atualizei a página com a rolagem de seções no scroll, fazendo uma animação

--- script.js
+++ script.js
@@ -3,5 +3,18 @@
         document.getElementById(id).style.display = 'none'
     } else {
         document.getElementById(id).style.display = 'block'
+        document.getElementById(id).style.transform = 'translateX(0)'
+        document.getElementById(id).style.transition = '.5s ease-in'
     }
 }
+
+/*Efeito de aparecer com a rolagem do SCROLL*/
+function ativaNoScrool(){
+    document.querySelectorAll('#hiddenScroll').forEach((hiddenScroll, index) => {
+    if(hiddenScroll.getBoundingClientRect().top < 450){
+            hiddenScroll.style.opacity= 1;
+            hiddenScroll.style.animation=" appear-left 1.4s";
+    };
+})
+}
+window.addEventListener('scroll', ativaNoScrool) //ativando a função na página
